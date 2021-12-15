a30e2609c31687fef71c5bcecc94bccf1393d5d1
subashbk <bksubhas418@gmail.com>
2021-12-15 13:57:07 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Intro from "./components/intro/Intro"
+import About from "./components/about/About"
+
+const App = () => {
+  return (
+    <div>  
+       <Intro/>
+      <About/>
+   </div>
+
+  )
+}
+
+export default App
---
+++ src/components/about/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./about.css";
+
+const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left"></div>
+            <div className="a-right"></div>
+
+
+
+        </div>
+    )
+}
+
+export default About;
